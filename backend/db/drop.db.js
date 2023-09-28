431364d66b1c8c977297f855c451e0928e021859
bonitoflakez <nikhildhiman9050@gmail.com>
2023-09-29 03:19:32 +0530

    [backend/db : feat] Automated table drop script

--- backend/db/drop.db.js
+++ backend/db/drop.db.js
@@ -1,44 +1,39 @@
-// drop table admin_users, branches, courses, faculty, roles, student, student_attendance_info, student_course_info, student_grade_info, student_info, student_personal_info;
-
 import { Pool } from "pg";
-import fs from "fs/promises";
-import path from "path";
 
 async function main() {
   try {
     const databaseConfig = {
-      user: Bun.env.DB_USER,
-      password: Bun.env.DB_PASSWD,
-      host: Bun.env.DB_HOST,
-      port: Bun.env.DB_PORT,
-      database: Bun.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
     };
 
     const pool = new Pool(databaseConfig);
 
-    const createTablesScript = await fs.readFile(
-      path.join(__dirname, "./schema/004_drop_tables.sql"),
-      "utf8"
+    const queryResult = await pool.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE'"
     );
 
-    const sqlStatements = createTablesScript.split(";");
+    const tableNames = queryResult.rows.map((row) => row.table_name);
 
-    for (let i = 0; i < sqlStatements.length; i++) {
-      const sqlStatement = sqlStatements[i].trim();
-      if (sqlStatement) {
-        try {
-          await pool.query(sqlStatement);
-        } catch (err) {
-          console.error(
-            `[!] Error executing SQL statement on line ${i + 1}: ${err.message}`
-          );
-          console.error(`Problematic SQL statement: ${sqlStatement}`);
-          return;
-        }
-      }
+    if (tableNames.length === 0) {
+      console.log("[+] No tables found to drop.");
+      pool.end();
+      return;
     }
 
-    console.log("[+] Dropped tables successfully.");
+    // Generate the DROP TABLE statement
+    const dropTablesQuery = `DROP TABLE IF EXISTS ${tableNames.join(", ")};`;
+
+    try {
+      await pool.query(dropTablesQuery);
+      console.log("[+] Dropped tables successfully.");
+    } catch (err) {
+      console.error("[!] Error dropping tables:", err.message);
+    }
+
     pool.end();
   } catch (err) {
     console.error("[!] Error initializing the database:", err);
